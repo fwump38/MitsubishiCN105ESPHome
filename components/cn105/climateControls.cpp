c55e825ca6f86135d1b04b75dcbfd2e2c22ed21f
fwump38 <github@slothcorp.com>
2025-05-13 17:14:38 -0600

    Update climateControls.cpp

--- components/cn105/climateControls.cpp
+++ components/cn105/climateControls.cpp
@@ -210,7 +210,7 @@ void CN105Climate::controlMode() {
 
 
 void CN105Climate::setActionIfOperatingTo(climate::ClimateAction action) {
-    if (this->currentSettings.stage && strcmp(this->currentSettings.stage, "IDLE") != 0) {
+    if (this->is_operating()) {
         this->action = action;
     } else {
         this->action = climate::CLIMATE_ACTION_IDLE;
